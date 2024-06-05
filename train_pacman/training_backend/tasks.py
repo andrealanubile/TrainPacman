fixed scaling issue

train_pacman/training_backend/tasks.py
@@ -272,7 +272,7 @@ def optimize_model():
 
     policy_net = DQN(state_dim, n_actions)
     target_net = DQN(state_dim, n_actions)
-    policy_net.load_state_dict(torch.load(os.path.join('training_backend', 'models', pretrain_checkpoint)))
+    policy_net.load_state_dict(torch.load(os.path.join('training_backend', 'models', pretrain_checkpoint), map_location=torch.device('cpu')))
     target_net.load_state_dict(policy_net.state_dict())
 
     buffer_policy_net = io.BytesIO()